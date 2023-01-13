0e7f5fafe64c824601eb6d7837a884e73565d157
LeoHaas <leo@kauz.ch>
2023-01-13 08:23:42 +0100

    init

---
+++ SoftwareTestsBeispiel/Average.cs
@@ -0,0 +1,26 @@
+﻿namespace SoftwareTestsBeispiel
+{
+    public class Average
+    {
+        public double Median(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+            {
+                throw new ArgumentException("Die Zahlenliste darf nicht leer sein.");
+            }
+
+            numbers.Sort();
+
+            int size = numbers.Count;
+            int middle = size / 2;
+
+            if (size % 2 != 0)
+            {
+                return numbers[middle];
+            }
+            double middle1 = numbers[middle - 1];
+            double middle2 = numbers[middle];
+            return (middle1 + middle2) / 2;
+        }
+    }
+}
---
+++ SoftwareTestsBeispiel/AverageTest.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace SoftwareTestsBeispiel
+{
+    public class AverageTest
+    {
+        [Fact]
+        public void TestMedianForOddNumbersOfElements()
+        {
+            List<int> numbers = new List<int> { 12, 100, 13, 1532, 1 };
+            double expected = 13;
+            Average average = new Average();
+
+            double actual = average.Median(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMedianForEvenNumbersOfElements()
+        {
+            List<int> numbers = new List<int> { 12, 100, 13, 1532, 1, 3 };
+            double expected = 12.5;
+            Average average = new Average();
+
+            double actual = average.Median(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMedianForNoElements()
+        {
+            List<int> numbers = new List<int> { };
+            Average average = new Average();
+
+            Assert.Throws<ArgumentException>(() => average.Median(numbers));
+        }
+    }
+}
